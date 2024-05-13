feb3366cc17e40bc5c7409ffc0fd7960dcc95bc0
Andrey Romanov <96443190+Qunexx@users.noreply.github.com>
2024-05-13 13:27:56 +0300

    Добавил авторизацию и аутентификацию

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function AuthStatus(){
-        return view("authcheck");
+        if(Auth::check()){
+            $status = 'Authentificated';
+        }
+        else{
+            $status = 'Not authentificated';
+        }
+        return view("authcheck",compact('status'));
     }
 }
--- app/Http/Controllers/MyPlaceController.php
+++ app/Http/Controllers/MyPlaceController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class MyPlaceController extends Controller
 {
-    public function hello(){
-        return ("Привет!");
+    public function index(){
+        return view('main');
     }
 }
