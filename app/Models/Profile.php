e8a7f3e259f93548eddd117c54bc045ef7a5c2dc
Andrey Romanov <96443190+Qunexx@users.noreply.github.com>
2024-05-18 17:55:44 +0300

    Добавил миграцию и модель профиля, а также настроил связь 1 к 1

---
+++ app/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    protected $table = 'profiles';
+    protected $fillable = ['birthday', 'quote', 'bio', 'vk', 'telegram','github'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
---
+++ database/migrations/2024_05_18_133108_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->date('birthday')->nullable();
+            $table->text('quote')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('vk')->nullable();
+            $table->string('telegram')->nullable();
+            $table->string('github')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
