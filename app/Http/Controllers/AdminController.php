69d66c0f292f5f405752d44be6476b78eecde9d8
Andrey Romanov <96443190+Qunexx@users.noreply.github.com>
2024-05-18 14:52:56 +0300

    Добавил статистику из бд в админку и поправил немного навигацию в админке

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
 public function index(){
-return view("layout.admin");
-}
-}
+    $postCount = Post::count();
+    $likesCount = Post::sum('likes');
+    $usersCount = User::count();
+return view("layout.admin",compact('postCount','likesCount','usersCount'));
+}}
+
+
